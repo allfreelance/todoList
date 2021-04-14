4718313657574f96c5467080a6a47ec884044797
nikolay_peremitin <peremitinns@mail.ru>
2021-04-14 17:57:23 +0300

    add gh-pages

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <nav class="bg-green-500 text-white">
-    <div class="container mx-auto p-2">
+    <div class="container mx-auto pl-5 pr-5">
       <div class="flex items-center">
         <router-link to="/" class="inline-block px-16 py-4 transition-colors hover:bg-green-700 duration-300">Home</router-link>
         <router-link to="todos" class="inline-block px-16 py-4 transition-colors hover:bg-green-700 duration-300">Todos</router-link>
